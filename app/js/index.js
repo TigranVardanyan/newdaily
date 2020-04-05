7fbeec75ed24142d112f993f4bf9f18844cfdc81
Tigran <tig.vardanyan.001@gmail.com>
2020-04-05 23:54:57 +0400

    Signed-off-by: Tigran <tig.vardanyan.001@gmail.com>
    some changes

--- app/js/index.js
+++ app/js/index.js
@@ -1,69 +1,65 @@
-// 'use strict'
-//
-//
-//
-//
-// class Homepage {
-//   myWindow = document.body;
-//   windowRotateDefaultParams = 'rotate3d(-0.5, 0.4, 0.3, 57deg)';
-//   windowRotateAlignScreen = 'rotate3d(0, 0, 0, 0deg)';
-//   rotated = true;
-//   transformOriginX = 50;
-//   transformOriginY = 50;
-//   constructor () {
-//     this.myWindow.style.transform = this.windowRotateDefaultParams;
-//     this.myWindow.style.transformOrigin = `${this.transformOriginX}% ${this.transformOriginY}%`;
-//     let iframes = this.myWindow.querySelectorAll("iframe");
-//     for(let i = 0; i < iframes.length; i++){
-//       iframes[i].style.pointerEvents = "none";
-//     }
-//   }
-//
-//   SwitchScreenPosition() {
-//     if (this.rotated == true) {
-//       this.myWindow.style.transform = this.windowRotateAlignScreen;
-//       this.rotated = false;
-//     } else {
-//       this.myWindow.style.transform = this.windowRotateDefaultParams;
-//       this.rotated = true;
-//     }
-//   }
-//   Movingcreen(e) {
-//     if(e.key == "ArrowRight" && this.transformOriginY < 100) {
-//       this.transformOriginY +=10;
-//       this.myWindow.style.transformOrigin = `${this.transformOriginX}% ${this.transformOriginY}%`;
-//     } else if (e.key == "ArrowLeft" && this.transformOriginY > 0) {
-//       this.transformOriginY -=10;
-//       this.myWindow.style.transformOrigin = `${this.transformOriginX}% ${this.transformOriginY}%`;
-//     }    else if (e.key == "ArrowUp" && this.transformOriginX < 100) {
-//       this.transformOriginX +=10;
-//       this.myWindow.style.transformOrigin = `${this.transformOriginX}% ${this.transformOriginY}%`;
-//     } else if (e.key == "ArrowDown" && this.transformOriginX > 0) {
-//       this.transformOriginX -=10;
-//       this.myWindow.style.transformOrigin = `${this.transformOriginX}% ${this.transformOriginY}%`;
-//     } else {
-//     }
-//   }
-// }
-//
-// let MyDaily = new Homepage();
-// MyDaily.myWindow.addEventListener('click', function() {
-//   MyDaily.SwitchScreenPosition();
-// });
-//
-// MyDaily.myWindow.addEventListener('keydown', function() {
-//   MyDaily.Movingcreen(event);
-// });
+'use strict'
 
 
-document.addEventListener("DOMContentLoaded", function() {
-  let x = document.querySelectorAll(".wrapper div")[13].offsetHeight;
-  let y = document.querySelectorAll(".wrapper div")[13].offsetWidth;
-  console.log(x);
-  console.log(y);
-  window.scrollTo({
-    top: 1000,
-    left: 500,
+window.onload = function() {
+  let y = document.querySelectorAll(".wrapper div")[12].offsetTop;
+  let x = document.querySelectorAll(".wrapper div")[12].offsetLeft;
+  //Why dont work without behavior
+  window.scroll({
+    top:y,
+    left:x,
     behavior: "smooth"
   })
+};
+
+///////////////////
+///////////////////
+////wheel event////
+///////////////////
+///////////////////
+
+let yy = document.querySelectorAll(".wrapper div")[12].offsetHeight;
+let xx = document.querySelectorAll(".wrapper div")[12].offsetWidth;
+
+console.log("xx " +xx);
+console.log("yy " + yy);
+
+let image_slider = document.querySelector(".main-content_image_slider");
+let scrollSizeY = 0;
+let scrollSizeX = 0;
+window.addEventListener("wheel", function(e) {
+  scrollSizeY = scrollSizeY + e.deltaY;
+  scrollSizeX = scrollSizeX + e.deltaX;
+  if(scrollSizeY > 900) {
+    scrollSizeY = 0;
+    console.log("up");
+    window.scrollBy({
+      top:yy,
+      // behavior: "smooth" //TODO
+    })
+  } else if (scrollSizeY < -900) {
+    console.log("down");
+    scrollSizeY = 0;
+    window.scrollBy({
+      top:-yy,
+      // behavior: "smooth" //TODO
+    })
+  }
+
+  if(scrollSizeX > 500) {
+    scrollSizeX = 0;
+    console.log("left");
+    window.scrollBy({
+      left:xx,
+      // behavior: "smooth" //TODO
+    })
+  } else if (scrollSizeX < -500) {
+    console.log("right");
+    scrollSizeX = 0;
+    window.scrollBy({
+      left:-xx,
+      // behavior: "smooth" //TODO
+    })
+  }
+  console.log(scrollSizeX)
 });
